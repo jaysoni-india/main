72d3e7f1c1c4a89e46a9d7bca50d52291d070c6e
Jay Soni <jaysoni@engineer.com>
2018-09-04 05:17:59 +0530

    ubuntu.sh added

---
+++ ubuntu.sh
@@ -0,0 +1,10 @@
+apt update
+apt dist-upgrade
+apt install apache2
+apt-get install python-software-properties
+apt install python
+apt install php
+add-apt-repository ppa:ondrej/php
+apt-get update
+apt install php7.2
+apt-get install php7.2-common php-pear php7.2-curl php7.2-dev php7.2-gd php7.2-mbstring php7.2-zip php7.2-mysql php7.2-xml
