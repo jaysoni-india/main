75deaf10800944ec90fe3e04a3da7f47fff2879c
Jay Soni <jaysoni@engineer.com>
2018-09-04 05:33:56 +0530

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -8,3 +8,4 @@ add-apt-repository ppa:ondrej/php
 apt-get update
 apt install php7.2
 apt-get install php7.2-common php-pear php7.2-curl php7.2-dev php7.2-gd php7.2-mbstring php7.2-zip php7.2-mysql php7.2-xml
+apt install zip unzip htop
